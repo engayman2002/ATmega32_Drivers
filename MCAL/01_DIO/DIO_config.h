cd2776217a61ea2d852b39d6fb149d9e3e453697
Ayman Elashry <engaymanvip@gmail.com>
2024-03-13 08:08:37 +0200

    My Config File

---
+++ MCAL/01_DIO/DIO_config.h
@@ -0,0 +1,12 @@
+/*
+                <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DIO_config.h >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                
+                Author: Ayman Mohamed Elasrhy
+                Layer : MCAL
+                SWC   : DIO/GPIO
+*/
+
+#ifndef _DIO_CONFIG_H_
+#define _DIO_CONFIG_H_
+
+#endif
